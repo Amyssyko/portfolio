f5f30784632400fb45b12ece1d4cc8b1c259a33e
Amyssyko <cristian-0005@live.com>
2024-10-23 18:39:59 -0500

    Actualizar tamaño de icono en BrowserIcon

--- src/components/ui/icon/browser-icon.tsx
+++ src/components/ui/icon/browser-icon.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 export const BrowserIcon: React.FC<IconProps> = (props) => (
 	<svg
 		xmlns='http://www.w3.org/2000/svg'
-		width='24'
-		height='24'
+		width='1em'
+		height='1em'
 		viewBox='0 0 24 24'
 		fill='none'
 		stroke='currentColor'
