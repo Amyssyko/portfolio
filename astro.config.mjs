95acb374a3cc6c5eecfa4779de19ccb3ad67e93c
Amyssyko <cristian-0005@live.com>
2026-09-16 22:48:49 -0500

    Update Astro configurations, upgrade dependencies, and adjust types in tsconfig.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'astro/config'
+import { defineConfig, memoryCache } from 'astro/config'
 import { loadEnv } from 'vite'
 
 import react from '@astrojs/react'
@@ -9,12 +9,15 @@ const env = loadEnv(process.env.NODE_ENV ?? 'development', process.cwd(), '')
 
 // https://astro.build/config
 export default defineConfig({
+	provider: memoryCache({ max: 500 }),
 	site: env.PUBLIC_SITE_URL,
 	devToolbar: {
 		enabled: false
 	},
 	vite: {
 		plugins: [tailwindcss()]
+	},cache:{
+
 	},
 	integrations: [sitemap(), react()]
 })
